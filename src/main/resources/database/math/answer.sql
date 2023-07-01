f54cee08411f21dc63447e89f9d7c16908759112
dipleks <gladiator9000@inbox.ru>
2023-07-02 00:54:06 +0300

    add db

---
+++ src/main/resources/database/math/answer.sql
@@ -0,0 +1,22 @@
+create table answer
+(
+    id       integer               not null
+        constraint answer_pk
+            primary key,
+    datetime varchar               not null,
+    number   integer               not null,
+    decision boolean default false not null
+);
+
+comment on table answer is 'Ответы пользователя';
+
+comment on column answer.datetime is 'Дата и время записи ответа на задачу';
+
+comment on column answer.number is 'Номер задачи';
+
+comment on column answer.decision is 'Ответ пользователя';
+
+alter table answer
+    owner to postgres;
+
+
---
+++ src/main/resources/database/math/task.sql
@@ -0,0 +1,25 @@
+create table task
+(
+    id             integer               not null
+        constraint id
+            primary key,
+    number         integer               not null,
+    condition      varchar               not null,
+    answer         varchar               not null,
+    answer_default boolean default false not null
+);
+
+comment on table task is 'Условия задач';
+
+comment on column task.number is 'Номер задачи';
+
+comment on column task.condition is 'Условие задачи';
+
+comment on column task.answer is 'Правильный ответ решенной задачи';
+
+comment on column task.answer_default is 'Ответ по умолчанию';
+
+alter table task
+    owner to postgres;
+
+
