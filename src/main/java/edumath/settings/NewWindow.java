3626a5ba77fd5e8916353447c3adb94e6299af78
dipleks <gladiator9000@inbox.ru>
2023-07-05 17:38:55 +0300

    add task

--- src/main/java/edumath/settings/NewWindow.java
+++ src/main/java/edumath/settings/NewWindow.java
@@ -3,10 +3,7 @@ package edumath.settings;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 import java.io.IOException;
 
 public enum NewWindow {
@@ -17,8 +14,6 @@ public enum NewWindow {
             FXMLLoader fxmlLoader = new FXMLLoader(Root.class.getResource(fxml));
             try {
                 Scene scene = new Scene(fxmlLoader.load());
-//                scene.setFill(Color.TRANSPARENT);
-//                window.initStyle(StageStyle.TRANSPARENT);
                 window.setScene(scene);
             } catch (IOException e) {
                 throw new RuntimeException(e);
